3cc990cebeefd1f009406bc2de69d81b23327c8b
MaksS161 <https://github.com/MaksS161>
2023-07-20 15:37:58 +0300

    commit

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задание 2
+Пользователь вводит с клавиатуры два числа. Первое
+число — это значение, второе число процент, который
+необходимо посчитать. Например, мы ввели с клавиатуры
+90 и 10. Требуется вывести на экран 10 процентов от 90.
+Результат: 9.
+ */
+
+double  res=0;
+Console.WriteLine("Введите Значение: ");
+var a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите процент: ");
+var procent = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"{procent} процентов от {a}");
+Console.WriteLine($"Результат {res = a * procent / 100}");
---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задание 3
+Пользователь вводит с клавиатуры четыре цифры.
+Необходимо создать число, содержащее эти цифры. На-
+пример, если с клавиатуры введено 1, 5, 7, 8 тогда нужно
+сформировать число 1578.
+ */
+Console.Write("Введите четре цифры ");
+var a = Convert.ToInt32(Console.Read());
+var b = Convert.ToInt32(Console.Read());
+var c = Convert.ToInt32(Console.Read());
+var d = Convert.ToInt32(Console.Read());
+
+Console.Write($"Введеное число {a=a}{b}{c}{d}" );
